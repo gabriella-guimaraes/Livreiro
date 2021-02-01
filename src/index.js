12237a0c0abf02b15b6f14d804891e68b464262f
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2021-02-01 16:22:06 -0300

    JS - chamada da API

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@
 const initBtn = document.querySelector("#initBtn");
 const home = document.querySelector(".home");
 const search = document.querySelector(".search");
+const placeHldr = "./img/capaNaoEncontrada.png"
+const infos = {
+  method: 'GET'
+}
 
 initBtn.addEventListener("click", () => {
+
   pageSearch()
   search.style.display = "block"
 })
@@ -15,10 +20,15 @@ function pageSearch() {
   const templateSearch = `
     <div class="header">
         <img src="./img/book.png" alt="">
+        <button id="returnBtn" >Voltar</button>
         <button class="backBtn">Sair</button>
     </div>
-    <input type="text" placeholder="Busque por Autor, Editora ou Título">
+    <input type="text" id="searchInput" placeholder="Busque por Autor, Editora ou Título">
     <button id="searchBook">Buscar</button>
+    <div id="searchResult">
+    </div>
+    <div id="individualBook">
+    </div>
     `
   search.innerHTML = templateSearch;
 
@@ -27,5 +37,75 @@ function pageSearch() {
     search.style.display = "none"
     home.style.display = "block"
   })
+
+  const searchResult = document.querySelector("#searchResult");
+  const searchInput = document.querySelector("#searchInput");
+  const searchBook = document.querySelector("#searchBook");
+  const bookLink = "https://www.googleapis.com/books/v1/volumes?q="
+
+  searchBook.addEventListener("click", () => {
+    
+    searchResult.innerHTML = "";
+    
+    const book = bookLink + searchInput.value.replace(" ", "-");
+
+    fetch(book, infos)
+      .then((response) => response.json())
+      .then((json) => {
+        searchInput.value= "";
+        console.log(json);
+        displayResults(json);
+
+        const eachCard = document.querySelectorAll(".eachBook");
+        eachCard.forEach((card) => {
+          card.addEventListener("click", (e) => {
+            showInfos(e)
+          })
+        });
+
+        const showInfos = (e) => {
+          const eachBook = e.target.parentNode;
+          fetch(eachBook.id, infos)
+            .then((response) => response.json())
+            .then((json) => {
+              console.log(json)
+
+              fetch("https://6015b2e155dfbd00174ca812.mockapi.io/api/v1/Livrarias", infos)
+                .then((response) => response.json())
+                .then((json) => {
+                  console.log(json)
+                })
+                .catch((erro) => console.log("Erro:" + erro));
+            })
+            .catch((erro) => console.log("Erro:" + erro));
+        }
+      })
+      .catch((erro) => console.log("Erro:" + erro));
+  })
 }
 
+function displayResults(response) {
+  for (let i = 0; i < response.items.length; i++) {
+    item = response.items[i];
+    title = item.volumeInfo.title;
+    author = item.volumeInfo.authors;
+    publisher = item.volumeInfo.publisher;
+    bookImg = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr;
+    selfLink = item.selfLink;
+
+    searchResult.innerHTML += formatOutput(title, author, publisher, bookImg, selfLink);
+  }
+};
+
+function formatOutput(title, author, publisher, bookImg, selfLink) {
+  var bookCard = `
+  <div class="eachBook" id="${selfLink}">
+    <img src="${bookImg}" alt="${title}">
+    <div id="${selfLink}">
+      <h2>${title}</h2>
+      <p>Autor: ${author}</p>
+      <p>Editora: ${publisher}</p>
+    </div>
+  </div>`
+  return bookCard;
+}
